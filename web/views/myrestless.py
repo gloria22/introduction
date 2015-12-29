0e3a596ce498ed0e2a9f0130908dce60b7eedff7
lichenyang <lichenyang@360.cn>
2015-12-29 11:51:07 +0800

    1. add preprocessor to restless

--- web/views/myrestless.py
+++ web/views/myrestless.py
@@ -1,7 +1,16 @@
 # -*- coding: utf8 -*-
-from flask_restless import APIManager
+from flask_restless import APIManager, ProcessingException
 from .. import app, db
 from ..models.Computermodel import Computer
+from flask import request
 
-manager = APIManager(app=app, flask_sqlalchemy_db=db)
+
+def req_check(*args, **kwargs):
+    if request.path.split("/")[-1].encode('utf-8').isdigit():
+        raise ProcessingException(description="get single", code=400)
+    else:
+        pass
+
+
+manager = APIManager(app=app, flask_sqlalchemy_db=db, preprocessors=dict(GET_SINGLE=[req_check], GET_MANY=[req_check]))
 manager.create_api(Computer, methods=['GET', 'POST', 'DELETE', 'PUT'])
